9780dd30ff4451d4976b93a194c49df3eb3ed9c1
yangchuanhu <ych_1024@163.com>
2020-12-01 14:20:37 +0800

    add etcd backup operator api

---
+++ api/v1alpha1/etcdbackup_types.go
@@ -0,0 +1,90 @@
+/*
+Copyright 2020 cnych.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	EtcdBackupPhaseBackingUp EtcdBackupPhase = "BackingUp"
+	EtcdBackupPhaseCompleted EtcdBackupPhase = "Completed"
+	EtcdBackupPhaseFailed    EtcdBackupPhase = "Failed"
+)
+
+type BackupStorageType string
+type EtcdBackupPhase string
+
+// EtcdBackupSpec defines the desired state of EtcdBackup
+type EtcdBackupSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Endpoints    []string          `json:"endpoints"`
+	StorageType  BackupStorageType `json:"storageType"`
+	BackupSource `json:",inline"`
+}
+
+type BackupSource struct {
+	S3  *S3BackupSource  `json:"s3,omitempty"`
+	OSS *OSSBackupSource `json:"oss,omitempty"`
+}
+
+type S3BackupSource struct {
+	Path string `json:"path"`
+	// Secret Object: AccessKey AcessSecryt
+	S3Secret string `json:"s3Secret"`
+}
+
+type OSSBackupSource struct {
+	Path      string `json:"path"`
+	OSSSecret string `json:"ossSecret"`
+}
+
+// EtcdBackupStatus defines the observed state of EtcdBackup
+type EtcdBackupStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Phase          EtcdBackupPhase `json:"phase,omitempty"`
+	StartTime      *metav1.Time    `json:"startTime,omitempty"`
+	CompletionTime *metav1.Time    `json:"completionTime,omitempty"`
+	//Condition
+}
+
+// +kubebuilder:object:root=true
+
+// EtcdBackup is the Schema for the etcdbackups API
+type EtcdBackup struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EtcdBackupSpec   `json:"spec,omitempty"`
+	Status EtcdBackupStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// EtcdBackupList contains a list of EtcdBackup
+type EtcdBackupList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []EtcdBackup `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&EtcdBackup{}, &EtcdBackupList{})
+}
